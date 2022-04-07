fe2174579089e575f4e805d0a999332f688190fe
Gabriel Rodrigues Rolim <100931450+rolim265@users.noreply.github.com>
2022-04-07 09:49:18 -0300

    trabalhogodofredoedabiblioteca!
    trabalho godofredo e da biblioteca! sql,caderno

---
+++ godofredo trabalho sql.sql
@@ -0,0 +1,13 @@
+insert  into pessoas
+(nome,idade,sexo,peso,altura,nacionalidade)
+values
+("Dolores",28,"f",50.62,1.52,"mexicana"),
+("Godolores",14,"f",30.25,1.50,"americana");
+
+select * from pessoas;
+
+insert  into pessoas
+(nome,idade,sexo,peso,altura,nacionalidade)
+values
+("Godofredo",95,"m",59.00,1.65,"Russo"),
+("Godolores",14,"f",30.25,1.50,"Ucraniana");
